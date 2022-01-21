ddc86014d1d34040c60b71dd628dec6e2ea396b8
drorspei <dror@pagaya.com>
2022-01-21 19:10:13 +0200

    no spark imports

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='splink',
+   version='1',
+   description='Splink',
+   author='Splink people',
+   packages=find_packages(),
+   install_requires=[]
+)
